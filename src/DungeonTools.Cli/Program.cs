cd18a023803735e06e0a82c11ca5a63993059910
Diego Rossi <git@hellpie.dev>
2020-06-02 01:22:51 +0200

    Add empty CLI project and global.json to define .NET Core runtime compatibility

---
+++ src/DungeonTools.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DungeonTools.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
