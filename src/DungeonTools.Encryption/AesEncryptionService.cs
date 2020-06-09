157bf55ad152b330484d6d9c4f14ff4cb1e7b6af
Diego Rossi <git@hellpie.dev>
2020-06-10 00:53:16 +0200

    Rewrite encryption and move it to a different package so that it can be easier to integrate with future architectural decisions

---
+++ src/DungeonTools.Encryption/AesEncryptionService.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace DungeonTools.Encryption {
+    public partial class AesEncryptionService : IEncryptionService {
+        /// <inheritdoc />
+        public Stream Decrypt(Stream encrypted) {
+            return Transform(encrypted, Algorithm.CreateDecryptor());
+        }
+
+        /// <inheritdoc />
+        public Stream Encrypt(Stream unencrypted) {
+            return Transform(unencrypted, Algorithm.CreateEncryptor());
+        }
+
+        private static Stream Transform(Stream input, ICryptoTransform transform) {
+            MemoryStream output = new MemoryStream();
+
+            using(CryptoStream crypto = new CryptoStream(input, transform, CryptoStreamMode.Read, true)) {
+                crypto.CopyTo(output);
+            }
+
+            output.Seek(0, SeekOrigin.Begin);
+            return output;
+        }
+    }
+}
---
+++ src/DungeonTools.Encryption/IEncryptionService.cs
@@ -0,0 +1,8 @@
+﻿using System.IO;
+
+namespace DungeonTools.Encryption {
+    public interface IEncryptionService {
+        Stream Decrypt(Stream encrypted);
+        Stream Encrypt(Stream unencrypted);
+    }
+}
