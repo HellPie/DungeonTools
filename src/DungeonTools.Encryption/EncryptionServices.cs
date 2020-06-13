01e4ef8ad51e37fcff5f1d1fb3e49cc5fb634d5f
Diego Rossi <git@hellpie.dev>
2020-06-14 00:22:02 +0200

    Implement helper class to provide all available services and automatically build a default based on keys existence

---
+++ src/DungeonTools.Encryption/EncryptionServices.cs
@@ -0,0 +1,17 @@
+﻿namespace DungeonTools.Encryption {
+    public class EncryptionServices {
+        public static readonly IEncryptionService Default;
+
+        public static readonly IEncryptionService LocalKeys = new AesEncryptionService();
+        public static readonly IEncryptionService RemoteApi = new ApiEncryptionService();
+
+
+        static EncryptionServices() {
+#if USE_KEYS
+            Default = LocalKeys;
+#else
+            Default = RemoteApi;
+#endif
+        }
+    }
+}
