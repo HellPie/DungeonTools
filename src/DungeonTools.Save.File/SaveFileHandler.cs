add9bd46063042774a6219dbf182f59c7e047433
Diego Rossi <git@hellpie.dev>
2020-06-24 18:33:14 +0200

    Cleanup code of commit #1 to conform to solution style

--- src/DungeonTools.Save.File/SaveFileHandler.cs
+++ src/DungeonTools.Save.File/SaveFileHandler.cs
@@ -9,49 +9,47 @@ namespace DungeonTools.Save.File {
         private static readonly byte[] Magic2 = {0x00, 0x00, 0x00, 0x00};
 
         public static bool IsFileEncrypted(Stream stream) {
-            if (!stream.CanRead) throw new ArgumentException();
-            if (!stream.CanSeek) throw new ArgumentException();
+            if(!stream.CanRead) throw new ArgumentException();
+            if(!stream.CanSeek) throw new ArgumentException();
 
-            if (StartsWithMagic(stream))
-            {
-                return true;
-            }
-            return false;
+            return StartsWithMagic(stream);
         }
 
-        public static bool StartsWithMagic(Stream stream)
-        {
-            if (stream.Length < Magic1.Length + Magic2.Length) return false;
+        public static bool StartsWithMagic(Stream stream) {
+            if(stream.Length < Magic1.Length + Magic2.Length) return false;
 
             long position = stream.Position;
-            var magic1 = new byte[Magic1.Length];
+            byte[] magic1 = new byte[Magic1.Length];
             stream.Read(magic1);
 
-            var magic2 = new byte[Magic2.Length];
+            byte[] magic2 = new byte[Magic2.Length];
             stream.Read(magic2);
 
-            if (!Magic1.SequenceEqual(magic1))
-            {
+            if(!Magic1.SequenceEqual(magic1)) {
                 stream.Position = position;
                 return false;
             }
-            if (!Magic2.SequenceEqual(magic2))
-            {
-                throw new Exception("file started with magic but next 4 bytes were non-zero");
+
+            if(!Magic2.SequenceEqual(magic2)) {
+                throw new Exception("File started with magic but next 4 bytes were non-zero");
             }
+
             return true;
         }
 
         public static Stream RemoveTrailingZeroes(Stream stream) {
-            using BinaryReader reader = new BinaryReader(stream, Encoding.ASCII, true); // this is not text
-            
+            using BinaryReader reader = new BinaryReader(stream, Encoding.ASCII, true); // This is not text
+
             while(reader.BaseStream.Position < reader.BaseStream.Length) {
-                if (reader.ReadByte() != 0) continue;
-                
-                // remove this zero and any after this. supposed to be text but the cipher adds some junk
-                stream.SetLength(reader.BaseStream.Position-1);
+                if(reader.ReadByte() != 0) {
+                    continue;
+                }
+
+                // Remove this zero and any after this. supposed to be text but the cipher adds some junk
+                stream.SetLength(reader.BaseStream.Position - 1);
                 break;
             }
+
             stream.Position = 0;
             return stream;
         }
